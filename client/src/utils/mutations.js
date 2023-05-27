533c08f705335a3598b099ce7a12cf221bdd4c1e
Kiggles <ridgewwoods@gmail.com>
2023-05-26 22:48:01 -0400

    working on mutations js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,23 +1,36 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-  mutation createMatchup($tech1: String!, $tech2: String!) {
-    createMatchup(tech1: $tech1, tech2: $tech2) {
-      _id
-      tech1
-      tech2
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
   }
 `;
 
 export const ADD_USER = gql`
-  mutation createVote($_id: String!, $techNum: Int!) {
-    createVote(_id: $_id, techNum: $techNum) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
+            email
+            savedBooks {
+                authors
+                bookId
+                image
+                link
+                title
+                description
+
+            }
+            
+        }
     }
   }
 `;
